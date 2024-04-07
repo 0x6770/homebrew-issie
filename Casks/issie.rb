8bf4eabf9c9bb5a1df3ef030904797b0c102fb71
YUJIE WANG <yujie@wang.icu>
2024-04-07 02:55:48 +0100

    update sha

--- Casks/issie.rb
+++ Casks/issie.rb
@@ -2,7 +2,7 @@ cask "issie" do
   arch arm: "arm64", intel: "x86_64"
 
   version "4.1.3"
-  sha256 arm:   "262d786c40f8f4a1925246f1ce5144b829693afb045caa34353b6c340aa1440f",
+  sha256 arm:   "e55f3cd9190711d26425319444d2eb33f286ed1435b0aad03644ae160196f3d9",
          intel: "c8edef9e1b036a82388e2893fcb38eeb7abc8b7bbdb5edb1233b9c1595dae1b7"
 
   url "https://github.com/tomcl/issie/releases/download/v#{version}/issie-#{version}-#{arch}.dmg",
